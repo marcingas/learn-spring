75a61e806963b914739451ba3c4d4d88f972fd8e
Marcin <gasiorek.mail@gmail.com>
2023-02-10 13:47:29 +0100

    add mapping

--- src/main/java/dev/Marcin/learnspring/CourseController.java
+++ src/main/java/dev/Marcin/learnspring/CourseController.java
@@ -1,6 +1,7 @@
 package dev.Marcin.learnspring;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -8,6 +9,7 @@ import java.util.List;
 
 @RestController
 public class CourseController {
+    @RequestMapping("/courses")
     public List<Course> retrieveAllCourses(){
         return Arrays.asList(
                 new Course(1,"Naucz się Spring Boot",  "dev.Marcin" ),
