36a993413e7a27bc24b9ca0d1266365e4e3c5fd2
Marcin <gasiorek.mail@gmail.com>
2023-02-10 13:45:04 +0100

    Initial commit

---
+++ src/main/java/dev/Marcin/learnspring/CourseController.java
@@ -0,0 +1,18 @@
+package dev.Marcin.learnspring;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+    public List<Course> retrieveAllCourses(){
+        return Arrays.asList(
+                new Course(1,"Naucz się Spring Boot",  "dev.Marcin" ),
+                new Course(2,"Naucz się Java z dev.Marcin", "dev.Marcin" ),
+                new Course(3,"Naucz się Collections z dev.Marcin", "dev.Marcin" )
+        );
+    }
+}
