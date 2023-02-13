55b46c3e72768497ab55d73b659800bf9c6dcc1c
Marcin <gasiorek.mail@gmail.com>
2023-02-13 13:24:13 +0100

    add application properties for different environment: production, dev, add one dependency

--- src/main/java/dev/Marcin/learnspring/Course.java
+++ src/main/java/dev/Marcin/learnspring/Course.java
@@ -1,34 +1,34 @@
 package dev.Marcin.learnspring;
 
 public class Course {
-    private long id;
-    private String name;
-    private String author;
+    public double id;
+    public String author;
+    public String title;
 
-    public Course(long id, String name, String author) {
-        this.id = id;
-        this.name = name;
-        this.author = author;
-    }
-
-    public long getId() {
+    public double getId() {
         return id;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public String getAuthor() {
         return author;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public Course(double id, String author, String title) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+    }
+
     @Override
     public String toString() {
         return "Course{" +
                 "id=" + id +
-                ", name='" + name + '\'' +
                 ", author='" + author + '\'' +
+                ", title='" + title + '\'' +
                 '}';
     }
 }
--- src/main/java/dev/Marcin/learnspring/CourseController.java
+++ src/main/java/dev/Marcin/learnspring/CourseController.java
@@ -4,17 +4,26 @@ package dev.Marcin.learnspring;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+
+
 
 @RestController
 public class CourseController {
     @RequestMapping("/courses")
-    public List<Course> retrieveAllCourses(){
+    public List<Course> retriveAllCoursese(){
         return Arrays.asList(
-                new Course(1,"Naucz się Spring Boot",  "dev.Marcin" ),
-                new Course(2,"Naucz się Java z dev.Marcin", "dev.Marcin" ),
-                new Course(3,"Naucz się Collections z dev.Marcin", "dev.Marcin" )
+                new Course(1,"Naucz się", "dev Martin"),
+                new Course(2, "How to learn Java", "dev Martin"),
+                new Course(3, "How to learn Spring", "dev Martin"),
+                new Course(4, "How to learn learning", "dev Martin"),
+                new Course(4, "How to learn learning", "dev Martin")
         );
     }
+
+
+
 }
