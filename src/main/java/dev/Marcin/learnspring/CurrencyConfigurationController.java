7aeb25c167a01758286205e252840639c7362ced
Marcin <gasiorek.mail@gmail.com>
2023-02-14 11:50:39 +0100

    adding configuration properties

---
+++ src/main/java/dev/Marcin/learnspring/CurrencyConfigurationController.java
@@ -0,0 +1,24 @@
+package dev.Marcin.learnspring;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@RestController
+public class CurrencyConfigurationController {
+
+    @Autowired
+    private CurrencyServiceConfiguration configuration;
+    @RequestMapping("/currency-configuration")
+    public CurrencyServiceConfiguration retriveAllCoursese(){
+        return configuration;
+    }
+
+
+
+}
